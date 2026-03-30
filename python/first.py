f69a7408669a38bffe854a55de8a9be4fa2832a5
Akshara123-web <aksharasrivastava018@gmail.com>
2026-03-30 22:45:21 +0530

    added new_web project

--- python/first.py
+++
@@ -1,8 +0,0 @@
-list =[]
-movie1=input("Enter 1st movie name : ") 
-movie2=input("Enter 2nd movie name : ")
-movie3=input("Enter 3rd movie name : ")
-list.append(movie1)
-list.append(movie2)
-list.append(movie3)
-print ("The movies are : ",list)
